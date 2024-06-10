37f96dceacd1767e0874ec79a69a509b7a7eb798
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-10 00:57:14 -0500

    single node broadcast

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 )
 
 type server struct {
-	id   string
-	node *maelstrom.Node
+	id    string
+	node  *maelstrom.Node
+	store []int
 }
 
 func newServer(id string, node *maelstrom.Node) *server {
-	return &server{id: id, node: node}
+	return &server{id: id, node: node, store: []int{}}
 }
 
 func (s *server) handleEcho(msg maelstrom.Message) error {
@@ -40,11 +41,47 @@ func (s *server) handleGenerate(msg maelstrom.Message) error {
 	return s.node.Reply(msg, body)
 }
 
+func (s *server) handleBroadcast(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	body["type"] = "broadcast_ok"
+	s.store = append(s.store, body["message"].(int))
+
+	return s.node.Reply(msg, body)
+}
+
+func (s *server) handleRead(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	body["type"] = "read_ok"
+	body["messages"] = s.store
+
+	return s.node.Reply(msg, body)
+}
+
+func (s *server) handleTopology(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	body["type"] = "topology_ok"
+	return s.node.Reply(msg, body)
+}
+
 func main() {
 	s := newServer(uuid.New().String(), maelstrom.NewNode())
 
 	s.node.Handle("echo", s.handleEcho)
 	s.node.Handle("generate", s.handleGenerate)
+	s.node.Handle("broadcast", s.handleBroadcast)
+	s.node.Handle("read", s.handleRead)
+	s.node.Handle("topology", s.handleTopology)
 
 	if err := s.node.Run(); err != nil {
 		log.Fatal(err)
