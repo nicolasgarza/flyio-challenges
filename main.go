cbd09a5ed6a8068b4aae9e1a7aca5d2cb317511e
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-09 23:29:42 -0500

    changed archictechhutechutre, not passing tests yet

--- main.go
+++ main.go
@@ -9,20 +9,41 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type server struct {
+	id   string
+	node *maelstrom.Node
+}
+
+func newServer(id string, node *maelstrom.Node) *server {
+	s := server{id: id, node: node}
+	return &s
+}
+
+func (s *server) handleEcho(body map[string]any, msg maelstrom.Message) (maelstrom.Message, map[string]any) {
+	// Update the message type to return back
+	body["type"] = "echo_ok"
+
+	return msg, body
+}
+
+func (s *server) handleGenerate(body map[string]any, msg maelstrom.Message) (maelstrom.Message, map[string]any) {
+	body["type"] = "generate_ok"
+	body["id"] = s.id
+
+	return msg, body
+}
+
 func main() {
 	n := maelstrom.NewNode()
+	s := newServer(uuid.New().String(), n)
+
 	n.Handle("echo", func(msg maelstrom.Message) error {
-		// Unmarshall the message body as an loosely-typed map.
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-
-		// Update the message type to return back
-		body["type"] = "echo_ok"
-
-		// Echo the original message back with the updated message type.
-		return n.Reply(msg, body)
+		newBody, newMsg := s.handleEcho(body, msg)
+		return n.Reply(newBody, newMsg)
 	})
 
 	n.Handle("generate", func(msg maelstrom.Message) error {
@@ -30,11 +51,8 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-
-		body["type"] = "generate_ok"
-		body["id"] = uuid.New()
-
-		return n.Reply(msg, body)
+		newBody, newMsg := s.handleGenerate(body, msg)
+		return n.Reply(newBody, newMsg)
 	})
 
 	if err := n.Run(); err != nil {
