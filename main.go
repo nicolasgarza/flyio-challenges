492e8c95d85daaf4edd4b3ba5093fd356eccca2a
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-10 19:49:12 -0500

    small refactor

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+const (
+	EchoType        = "echo"
+	EchoOkType      = "echo_ok"
+	GenerateType    = "generate"
+	GenerateOkType  = "generate_ok"
+	BroadcastType   = "broadcast"
+	BroadcastOkType = "broadcast_ok"
+	ReadType        = "read"
+	ReadOkType      = "read_ok"
+	TopologyType    = "topology"
+	TopologyOkType  = "topology_ok"
+)
+
 type server struct {
 	id       string
 	node     *maelstrom.Node
@@ -26,7 +39,7 @@ func (s *server) handleEcho(msg maelstrom.Message) error {
 		return err
 	}
 	// Update the message type to return back
-	body["type"] = "echo_ok"
+	body["type"] = EchoOkType
 
 	return s.node.Reply(msg, body)
 }
@@ -36,7 +49,7 @@ func (s *server) handleGenerate(msg maelstrom.Message) error {
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
-	body["type"] = "generate_ok"
+	body["type"] = GenerateOkType
 	body["id"] = uuid.New().String()
 
 	return s.node.Reply(msg, body)
@@ -48,21 +61,17 @@ func (s *server) handleBroadcast(msg maelstrom.Message) error {
 		return err
 	}
 
-	body["type"] = "broadcast_ok"
+	body["type"] = BroadcastOkType
 	recieved_int := int(body["message"].(float64))
 
-	if _, exists := s.store[recieved_int]; exists {
+	if s.isMessageReceived(recieved_int) {
 		// early return if we have already received int, don't broadcast
-		delete(body, "message")
-		return s.node.Reply(msg, body)
+		return s.replyBroadcastOk(msg, body)
 	}
 
-	s.store[recieved_int] = struct{}{}
-	s.received = append(s.received, recieved_int)
+	s.storeMessage(recieved_int)
 	s.broadcastMsg(recieved_int)
-	delete(body, "message")
-
-	return s.node.Reply(msg, body)
+	return s.replyBroadcastOk(msg, body)
 }
 
 func (s *server) handleRead(msg maelstrom.Message) error {
@@ -71,7 +80,7 @@ func (s *server) handleRead(msg maelstrom.Message) error {
 		return err
 	}
 
-	body["type"] = "read_ok"
+	body["type"] = ReadOkType
 	body["messages"] = s.received
 
 	return s.node.Reply(msg, body)
@@ -83,14 +92,14 @@ func (s *server) handleTopology(msg maelstrom.Message) error {
 		return err
 	}
 
-	body["type"] = "topology_ok"
-	delete(body, "topology")
+	body["type"] = TopologyOkType
+	delete(body, TopologyType)
 	return s.node.Reply(msg, body)
 }
 
 func (s *server) broadcastMsg(msg int) {
 	send_msg := map[string]any{
-		"type":    "broadcast",
+		"type":    BroadcastType,
 		"message": msg,
 	}
 	for _, n := range s.node.NodeIDs() {
@@ -98,14 +107,29 @@ func (s *server) broadcastMsg(msg int) {
 	}
 }
 
+func (s *server) storeMessage(msg int) {
+	s.store[msg] = struct{}{}
+	s.received = append(s.received, msg)
+}
+
+func (s *server) isMessageReceived(msg int) bool {
+	_, exists := s.store[msg]
+	return exists
+}
+
+func (s *server) replyBroadcastOk(msg maelstrom.Message, body map[string]any) error {
+	delete(body, "message")
+	return s.node.Reply(msg, body)
+}
+
 func main() {
 	s := newServer(uuid.New().String(), maelstrom.NewNode())
 
-	s.node.Handle("echo", s.handleEcho)
-	s.node.Handle("generate", s.handleGenerate)
-	s.node.Handle("broadcast", s.handleBroadcast)
-	s.node.Handle("read", s.handleRead)
-	s.node.Handle("topology", s.handleTopology)
+	s.node.Handle(EchoType, s.handleEcho)
+	s.node.Handle(GenerateType, s.handleGenerate)
+	s.node.Handle(BroadcastType, s.handleBroadcast)
+	s.node.Handle(ReadType, s.handleRead)
+	s.node.Handle(TopologyType, s.handleTopology)
 
 	if err := s.node.Run(); err != nil {
 		log.Fatal(err)
